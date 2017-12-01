ec7652cdb4d6c5cfa47a7595a42dc0e21dd7e24c
rbobko <rbobko@pgs-soft.com>
2017-11-30 21:05:32 -0500

    Getting bean from Spring context

--- src/main/java/com/rbobko93/spring5di/Spring5diApplication.java
+++ src/main/java/com/rbobko93/spring5di/Spring5diApplication.java
@@ -1,12 +1,18 @@
 package com.rbobko93.spring5di;
 
+import com.rbobko93.spring5di.controller.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Spring5diApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring5diApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(Spring5diApplication.class, args);
+
+		MyController controller = (MyController) ctx.getBean("myController");
+
+		controller.hello();
 	}
 }
---
+++ src/main/java/com/rbobko93/spring5di/controller/MyController.java
@@ -0,0 +1,11 @@
+package com.rbobko93.spring5di.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public void hello(){
+        System.out.println("Hello!");
+    }
+}
