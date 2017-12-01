afd0743166bed37b4750bc70fafac8245b0aec3a
rbobko <rbobko@pgs-soft.com>
2017-11-30 20:57:45 -0500

    Initial commit

---
+++ src/main/java/com/rbobko93/spring5di/Spring5diApplication.java
@@ -0,0 +1,12 @@
+package com.rbobko93.spring5di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5diApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5diApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rbobko93/spring5di/Spring5diApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rbobko93.spring5di;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5diApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
