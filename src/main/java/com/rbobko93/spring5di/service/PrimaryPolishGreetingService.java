cabd738111fd70ff82aee5bb83605122aa31e6e6
rbobko <rbobko@pgs-soft.com>
2017-12-10 14:06:07 -0500

    Adding spring profiles

--- src/main/java/com/rbobko93/spring5di/service/PrimaryPolishGreetingService.java
+++ src/main/java/com/rbobko93/spring5di/service/PrimaryPolishGreetingService.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("pl")
 @Primary
-@Profile("en")
-public class PrimaryGreetingService implements GreetingService {
+public class PrimaryPolishGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greeting service";
+        return "Witaj - Podstawowy serwis";
     }
 }
